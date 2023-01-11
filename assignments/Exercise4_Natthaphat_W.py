7205741728f01c8f647e9c27a9294313500ce932
JoeChangmin <natthaphat.wi@gmail.com>
2023-01-11 21:37:04 +0700

    Create Exercise4_Natthaphat_W.py

---
+++ assignments/Exercise4_Natthaphat_W.py
@@ -0,0 +1,10 @@
+fundEng = 88.5
+genBusi = 90.0
+introCS = 87.5
+comPro = 95.0
+
+print("--- Your Score ---")
+print("Foundation English :", fundEng)
+print("General Business :", genBusi)
+print("Introduction to Computer Systems :", introCS)
+print("Computer Programming :", comPro)
