ec4000d10f68154629a1f93a890d867692f25c1c
JoeChangmin <natthaphat.wi@gmail.com>
2023-01-12 23:53:17 +0700

    sent Lecture50

---
+++ assignments/Lecture50_Natthaphat_W.py
@@ -0,0 +1,14 @@
+def addNumber(x,y):
+    print(x,"+",y,"=", x+y)
+def deleteNumber(x,y):
+    print(x,"-",y,"=",x-y)
+def multipleNumber(x,y):
+    print(x,"x",y,"=",x*y)
+def divideNumber(x,y):
+    print(x,"/",y,"=",x/y)
+
+addNumber(5,8)
+deleteNumber(10,8)
+multipleNumber(5,6)
+divideNumber(8,5)
+
