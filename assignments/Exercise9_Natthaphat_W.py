2c90d7b7a1883b493b06b7eb283fd420281c5f4a
JoeChangmin <natthaphat.wi@gmail.com>
2023-01-12 16:18:34 +0700

    sent Exercise9

---
+++ assignments/Exercise9_Natthaphat_W.py
@@ -0,0 +1,8 @@
+userName = input("User Name : ")
+password = input("Password : ")
+
+while userName != "abc" or password != "1234":
+    userName = input("User Name : ")
+    password = input("Password : ")
+
+print("Done !!!")
